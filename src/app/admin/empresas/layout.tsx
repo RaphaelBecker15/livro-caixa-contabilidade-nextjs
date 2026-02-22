b79f97a7cf8b600ab38357dd618ad10223255498
Raphael Becker <rraffaelbeckerr@gmail.com>
2026-02-22 12:08:24 -0300

    Ajustes em layouts

--- src/app/admin/empresas/layout.tsx
+++ src/app/admin/empresas/layout.tsx
@@ -1,10 +1,5 @@
-import { EmpresasProvider } from "@/contexts/admin/ApiEmpresasContext";
 import { ReactNode } from "react";
 
 export default function EmpresasLayout({ children }: { children: ReactNode }) {
-    return (
-        <EmpresasProvider>
-            {children}
-        </EmpresasProvider>
-    )
+    return <>{children}</>
 }
--- src/app/admin/usuarios/layout.tsx
+++ src/app/admin/usuarios/layout.tsx
@@ -1,10 +1,5 @@
-import { UsuariosProvider } from "@/contexts/admin/ApiUsuariosContext";
 import { ReactNode } from "react";
 
 export default function UsuariosLayout({ children }: { children: ReactNode }) {
-    return (
-        <UsuariosProvider>
-            {children}
-        </UsuariosProvider>
-    )
+    return <>{children}</>
 }
