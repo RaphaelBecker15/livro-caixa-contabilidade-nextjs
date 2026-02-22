a1d414736ac7ae50c106a634e7a80c809f7ebb1a
Raphael Becker <rraffaelbeckerr@gmail.com>
2026-02-22 12:19:11 -0300

    Adição de SpeedInsights para produção

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="pt-br">
       <body>
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
