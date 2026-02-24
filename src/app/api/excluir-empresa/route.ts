76df9c54e83079617ec512f7fd148098de22e9ef
Raphael Becker <rraffaelbeckerr@gmail.com>
2026-02-24 11:40:55 -0300

    Ajuste de codigo

--- src/app/api/excluir-empresa/route.ts
+++ src/app/api/excluir-empresa/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     try {
         const body = await request.json()
-        const { id } = await request.json()
+        const { id } = body
 
         const cookieStore = await cookies()
         const supabaseClient = createServerClient(
